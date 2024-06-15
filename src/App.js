a3c2b09231854451f019319164408f9c833516f1
Nagaraj <7646547+nagaraj-nk@users.noreply.github.com>
2024-06-15 06:21:25 +0530

    id: 5c8f3e68-187e-4f4d-95c1-af453b1dc046, timestamp: 1718412685.243

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import { HomePage } from "./components/router-pages/HomePageRouter";
+import ParticlesBackground from "./ParticlesBackground";
 function App() {
   return (
     <div className="continer-fluid">
-      <HomePage />
+      <ParticlesBackground />
     </div>
   );
 }
