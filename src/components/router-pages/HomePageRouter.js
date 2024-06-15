345eed1a350831c5bed2ae8d34d1b7809e18e619
Nagaraj <7646547+nagaraj-nk@users.noreply.github.com>
2024-06-15 10:10:55 +0530

    id: 2a1e1be8-60bd-482b-b3ac-75fcbd6b4fdc, timestamp: 1718426455.482178

--- src/components/router-pages/HomePageRouter.js
+++ src/components/router-pages/HomePageRouter.js
@@ -1,9 +1,8 @@
 import { React, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { NavHeader } from "../ui/HeaderAndPage";
-import { BlogsIndexPage, LoginPage } from "./LoginPage";
-import { AnotherBlogPage, ProductsPage } from "./ProductsPage";
-import { FetchAPIPage } from "./FetchAPIPage";
+import { LoginPage } from "./LoginPage";
+import { ProductsPage } from "./ProductsPage";
 import { AdminPage } from "./Admin";
 export function HomePage() {
   return (
@@ -13,7 +12,6 @@ export function HomePage() {
           <Route index element={<LoginPage />} />
           <Route path="/products" element={<ProductsPage />} />
           <Route path="/admin" element={<AdminPage />} />
-          <Route path="/test/fetchapi/:id" element={<FetchAPIPage />} />
         </Route>
       </Routes>
     </div>
